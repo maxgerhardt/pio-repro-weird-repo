c2484235ba29e805f5dc1ceca2ed2192dc3a78c1
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-12-12 21:14:49 +0100

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <OpenTherm.h>
+#include <ArduinoMqttClient.h>
+#include <ESPTelnet.h>
+#include <FileData.h>
+#include <GyverPID.h>
+#include <Blinker.h>
+#include <DallasTemperature.h>
+#include <TinyLogger.h>
+
+void setup() {}
+void loop() {}
